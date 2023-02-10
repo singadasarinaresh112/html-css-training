ac3d7b8cb8065840c5c32ce69f541edf43dd5e16
Naresh <singadasarinaresh112@gmail.com>
2023-02-09 22:02:16 -0600

    Otp001 updating assign13

--- All_assignments/assignment13.js
+++ All_assignments/assignment13.js
@@ -19,3 +19,23 @@ function divideBy(){
 
 //   console.log(getWebsiteURL());
 
+const input = 'kayak'
+const form = document.querySelector('.palindrome__form')
+
+function palindromeToVerify(value) {
+ return value == reverseString(value) ? true : false
+}
+
+function reverseString(str) {
+    const stringToArray = str.split("")
+    const reverseArray = stringToArray.reverse()
+    const joinArray = reverseArray.join("")
+    return joinArray;
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const value = form.querySelector('#word').value
+  document.querySelector('#return').innerHTML = palindromeToVerify(value)
+})
+
